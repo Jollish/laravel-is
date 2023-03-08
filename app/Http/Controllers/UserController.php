3daab9201d756a37393b6ad941537f621b33bfa7
Jollish <106536867+Jollish@users.noreply.github.com>
2023-03-08 18:35:32 +0800

    Update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,14 @@ class UserController extends Controller
 
     public function store(Request $req)
     {
-        @dd($req);
+        $validated=$req->validate([
+            "name"=>['required', 'min:4'],
+            "email"=>['required', 'email', Rule::unique('users','email')],
+            "password"=>'required|confirmed|min:6'
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+        $user=User::create($validated);
+        return redirect("/");
     } 
 }
